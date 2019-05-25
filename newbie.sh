4589a7b4f1828c91c39a29dfd7e3f322966d3751
sixtysix-Team <50753116+sixtysix-Team@users.noreply.github.com>
2019-05-25 17:32:28 +0700

    Add files via upload

---
+++ newbie.sh
@@ -0,0 +1,471 @@
+#!/bin/bash
+#version 1.0
+#Mao Ngapain?
+#Mao Recode?
+#Utamakan Izin Dulu^_^
+clear
+#variabels
+b='\033[34;1m'
+g='\033[32;1m'
+p='\033[35;1m'
+c='\033[36;1m'
+r='\033[31;1m'
+w='\033[37;1m'
+y='\033[33;1m'
+sleep 1
+
+echo
+echo
+echo
+echo
+toilet -f future " SELAMAT DATANG BOSSKUH " | lolcat
+sleep 5
+clear
+toilet -f standard " ACG606 " -F gay
+echo $g"[+]"$r"▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼"$g"[+]"
+echo $g" ▶ "$c"      ◆◆◆◆◆◆      ♛606_HackTeam♛      ◆◆◆◆◆◆       "$g " ◀ "
+echo $g" ▶ "$r"▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼"$g "◀ "
+echo $g" ▶ "$w" Author      : "$y" ACG606                                 "$g" ◀ "
+echo $g" ▶ "$w" No WhatsApp : "$y" ☏ 081219****** ☏                          "$g" ◀ "
+echo $g" ▶ "$w" Youtube     : "$y" Hack 606        "$g"                          ◀ "
+echo $g"[+]"$r"▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲"$g"[+]"
+
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo -b $g"[#]> (Ctrl + C ) Detected, Trying To Exit ...."
+echo -b $g"[#]> GUNAKAN TOOLS INI DENGAN BIJAK BOOSSKU "
+sleep 1
+echo ""
+echo -b $g"[#]> ACG606"
+
+echo -b $g"[#]> See you Again :)..."
+sleep 1
+exit
+}
+
+lagi=1
+while [ $lagi -lt 6 ];
+do
+echo""
+echo""
+echo""
+echo $c"1. BruteForce Facebook MBF "
+echo $c"■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo $r"2. BruteForce Facebook ₪ "
+echo $r"■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo $g"3. Cloning Yahoo "
+echo $g"■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo $y"4. Bot Komen Facebook "
+echo $y"■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo $b"5. Auto Report Facebook "
+echo $b"■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo $g"6. Profile Guard Facebook "
+echo $g"■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo $r"00. Keluar "
+echo $g
+read -p " Pilih No : " pil;
+
+case $pil in
+1) clear
+toilet -f standard " ACG606 " -F gay
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+
+;;
+
+2) clear
+toilet -f standard " ACG606 " -F gay
+git clone https://github.com/IqbalzNoobs/fb-brute
+cd fb-brute
+python2 brute.py
+
+;;
+
+3) clear
+toilet -f standard " ACG606 " -F gay
+git clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+
+;;
+
+4) clear
+toilet -f standard " ACG606 " -F gay
+pip2 install mechanize
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+
+;;
+
+5) clear
+toilet -f standard " ACG606 " -F gay
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+
+;;
+
+6) clear
+toilet -f standard " ACG606 " -F gay
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
+
+;;
+
+00) toilet -f standard " ACG606 " -F gay
+echo $g"Keluar....."
+exit
+;;
+
+esac
+done
+done
+
+
+■■■■■■■■■■■■■■■■"
+echo $r"00. Keluar "
+echo $g
+read -p " root@ACG606:~# " pil;
+
+case $pil in
+1) clear
+toilet -f standard " ACG606 " -F gay
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+
+;;
+
+2) clear
+toilet -f standard " ACG606 " -F gay
+git clone https://github.com/IqbalzNoobs/fb-brute
+cd fb-brute
+python2 brute.py
+
+;;
+
+3) clear
+toilet -f standard " ACG606 " -F gay
+git clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+
+;;
+
+4) clear
+toilet -f standard " ACG606 " -F gay
+pip2 install mechanize
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+
+;;
+
+5) clear
+toilet -f standard " ACG606 " -F gay
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+
+;;
+
+6) clear
+toilet -f standard " ACG606 " -F gay
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
+
+;;
+
+00) toilet -f standard " AVG606 " -F gay
+echo $g"Keluar....."
+exit
+;;
+
+esac
+done
+done
+
+
+■■■■■■■■■■■■■■■■"
+echo $r"00. Keluar "
+echo $g
+read -p " root@ACG606:~# " pil;
+
+case $pil in
+1) clear
+toilet -f standard " ACG606 " -F gay
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+
+;;
+
+2) clear
+toilet -f standard " ACG606 " -F gay
+git clone https://github.com/IqbalzNoobs/fb-brute
+cd fb-brute
+python2 brute.py
+
+;;
+
+3) clear
+toilet -f standard " ACG606 " -F gay
+git clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+
+;;
+
+4) clear
+toilet -f standard " ACG606 " -F gay
+pip2 install mechanize
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+
+;;
+
+5) clear
+toilet -f standard " ACG606 " -F gay
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+
+;;
+
+6) clear
+toilet -f standard " ACG606 " -F gay
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
+
+;;
+
+00) toilet -f standard " ACG606 " -F gay
+echo $g"Keluar....."
+exit
+;;
+
+esac
+done
+done
+
+
+■■■■■■■■■■■■■■■■"
+echo $r"00. Keluar "
+echo $g
+read -p " root@ACG606:~# " pil;
+
+case $pil in
+1) clear
+toilet -f standard " ACG606 " -F gay
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+
+;;
+
+2) clear
+toilet -f standard " ACG606 " -F gay
+git clone https://github.com/IqbalzNoobs/fb-brute
+cd fb-brute
+python2 brute.py
+
+;;
+
+3) clear
+toilet -f standard " ACG606 " -F gay
+git clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+
+;;
+
+4) clear
+toilet -f standard " ACG606 " -F gay
+pip2 install mechanize
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+
+;;
+
+5) clear
+toilet -f standard " ACG606 " -F gay
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+
+;;
+
+6) clear
+toilet -f standard " ACG606 " -F gay
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
+
+;;
+
+00) toilet -f standard " ACG606 " -F gay
+echo $g"Keluar....."
+exit
+;;
+
+esac
+done
+done
+
+
+■■■■■■■■■■■■■■■■"
+echo $r"00. Keluar "
+echo $g
+read -p " root@ACG606:~# " pil;
+
+case $pil in
+1) clear
+toilet -f standard " ACG606 " -F gay
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+
+;;
+
+2) clear
+toilet -f standard " ACG606 " -F gay
+git clone https://github.com/IqbalzNoobs/fb-brute
+cd fb-brute
+python2 brute.py
+
+;;
+
+3) clear
+toilet -f standard " ACG606 " -F gay
+git clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+
+;;
+
+4) clear
+toilet -f standard " ACG606 " -F gay
+pip2 install mechanize
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+
+;;
+
+5) clear
+toilet -f standard " ACG606 " -F gay
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+
+;;
+
+6) clear
+toilet -f standard " ACG606 " -F gay
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
+
+;;
+
+00) toilet -f standard " ACG606 " -F gay
+echo $g"Keluar....."
+exit
+;;
+
+esac
+done
+done
+
+
+■■■■■■■■■■■■■■■■"
+echo $r"00. Keluar "
+echo $g
+read -p " root@ACG606:~# " pil;
+
+case $pil in
+1) clear
+toilet -f standard " ACG606 " -F gay
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+
+;;
+
+2) clear
+toilet -f standard " ACG606 " -F gay
+git clone https://gitoobs/fb-brute
+cd fb-brute
+python2 brute.py
+
+;;
+
+3) clear
+toilet -f standard " ACG606 " -F gay
+git clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+
+;;
+
+4) clear
+toilet -f standard " ACG606 " -F gay
+pip2 install mechanize
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+
+;;
+
+5) clear
+toilet -f standard " ACG606 " -F gay
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+
+;;
+
+6) clear
+toilet -f standard " ACG606 " -F gay
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
+
+;;
+
+00) toilet -f standard " ACG606 " -F gay
+echo $g"Keluar....."
+exit
+;;
+
+esac
+done
+done
+
+
+■■■■■■■■■■■■■■■■"
+echo $r"00. Keluar "
+echo $g
+read -p " root@ACG606:~# " pil;
+
+case $pil in
+1) clear
+toilet -f standard " ACG606 " -F gay
+git clone https://github.co
