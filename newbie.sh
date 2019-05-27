890fc7b68b3f98f260465e0ce224011ffc65e835
sixtysix-Team <50753116+sixtysix-Team@users.noreply.github.com>
2019-05-27 20:03:36 +0700

    Create newbie.sh

---
+++ newbie.sh
@@ -0,0 +1,659 @@
+#!/bin/bash
+#version 1.0
+#Mao Ngapain?
+#Mao Recode?
+#Utamakan Izin Dulu^_^
+clear
+#variabels
+b='\033[34;1m'
+g='\033[32;1m'
+p='\033[35;1m'
+c='\033[36;1m'
+r='\033[31;1m'
+w='\033[37;1m'
+y='\033[33;1m'
+sleep 1
+clear
+sleep 1
+echo $c"♥ LOADING ♥"
+sleep 3
+clear
+toilet -f future "welcome" | lolcat
+sleep 5
+clear
+toilet -f standard " BLANK " -F gay
+sleep 2
+clear
+
+echo $y"∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞
+
+                 [ fb tools ]
+
+♦ Coded   : BLANK
+♦ Team    : sixtysix hackteam
+♦ Github  : https://github.com/sixtysix-Team
+♦ Youtube : hack 606
+
+∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
+
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo -b $g"[#]> (Ctrl + C ) Detected, Trying To Exit ...."
+echo -b $g"[#]> GUNAKAN TOOLS INI DENGAN BIJAK BOOSSKU "
+sleep 1
+echo ""
+echo -b $g"[#]> BLANK"
+
+echo -b $g"[#]> See you Again :)..."
+sleep 1
+exit
+}
+
+lagi=1
+while [ $lagi -lt 6 ];
+do
+echo""
+echo""
+echo""
+echo $c"1. BruteForce Facebook MBF "
+echo $c"∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
+echo $r"2. BruteForce Facebook ₪ "
+echo $r"∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
+echo $g"3. Cloning Yahoo "
+echo $g"∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
+echo $y"4. Bot Komen Facebook "
+echo $y"∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
+echo $b"5. Auto Report Facebook "
+echo $b"∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
+echo $g"6. Profile Guard Facebook "
+echo $g"∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
+echo $r"00. Keluar "
+echo $g
+read -p " Pilih No : " pil;
+
+case $pil in
+1) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+
+;;
+
+2) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/IqbalzNoobs/fb-brute
+cd fb-brute
+python2 brute.py
+
+;;
+
+3) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+
+;;
+
+4) clear
+toilet -f standard " BLANK " -F gay
+pip2 install mechanize
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+
+;;
+
+5) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+
+;;
+
+6) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
+
+;;
+
+00) toilet -f standard " BLANK " -F gay
+echo $g"Keluar....."
+exit
+;;
+
+esac
+done
+done
+
+
+■■■■■■■■■■■■■■■■"
+echo $r"00. Keluar "
+echo $g
+read -p " root@BLANK:~# " pil;
+
+case $pil in
+1) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+
+;;
+
+2) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/IqbalzNoobs/fb-brute
+cd fb-brute
+python2 brute.py
+
+;;
+
+3) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+
+;;
+
+4) clear
+toilet -f standard " BLANK " -F gay
+pip2 install mechanize
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+
+;;
+
+5) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+
+;;
+
+6) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
+
+;;
+
+00) toilet -f standard " BLANK " -F gay
+echo $g"Keluar....."
+exit
+;;
+
+esac
+done
+done
+
+
+■■■■■■■■■■■■■■■■"
+echo $r"00. Keluar "
+echo $g
+read -p " root@BLANK:~# " pil;
+
+case $pil in
+1) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+
+;;
+
+2) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/IqbalzNoobs/fb-brute
+cd fb-brute
+python2 brute.py
+
+;;
+
+3) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+
+;;
+
+4) clear
+toilet -f standard " BLANK " -F gay
+pip2 install mechanize
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+
+;;
+
+5) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+
+;;
+
+6) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
+
+;;
+
+00) toilet -f standard " BLANK " -F gay
+echo $g"Keluar....."
+exit
+;;
+
+esac
+done
+done
+
+■■■■■■■■■■■■■■■■"
+echo $r"00. Keluar "
+echo $g
+read -p " root@BLANK:~# " pil;
+
+case $pil in
+1) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+
+;;
+
+2) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/IqbalzNoobs/fb-brute
+cd fb-brute
+python2 brute.py
+
+;;
+
+3) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+
+;;
+
+4) clear
+toilet -f standard " BLANK " -F gay
+pip2 install mechanize
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+
+;;
+
+5) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+
+;;
+
+6) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
+
+;;
+
+00) toilet -f standard " BLANK " -F gay
+echo $g"Keluar....."
+exit
+;;
+
+esac
+done
+done
+
+■■■■■■■■■■■■■■■■"
+echo $r"00. Keluar "
+echo $g
+read -p " root@BLANK:~# " pil;
+
+case $pil in
+1) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+
+;;
+
+2) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/IqbalzNoobs/fb-brute
+cd fb-brute
+python2 brute.py
+
+;;
+
+3) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+
+;;
+
+4) clear
+toilet -f standard " BLANK " -F gay
+pip2 install mechanize
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+
+;;
+
+5) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+
+;;
+
+6) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
+
+;;
+
+00) toilet -f standard " BLANK " -F gay
+echo $g"Keluar....."
+exit
+;;
+
+esac
+done
+done
+
+■■■■■■■■■■■■■■■■"
+echo $r"00. Keluar "
+echo $g
+read -p " root@BLANK:~# " pil;
+
+case $pil in
+1) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+
+;;
+
+2) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/IqbalzNoobs/fb-brute
+cd fb-brute
+python2 brute.py
+
+;;
+
+3) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+
+;;
+
+4) clear
+toilet -f standard " BLANK " -F gay
+pip2 install mechanize
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+
+;;
+
+5) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+
+;;
+
+6) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
+
+;;
+
+00) toilet -f standard " BLANK " -F gay
+echo $g"Keluar....."
+exit
+;;
+
+esac
+done
+done
+
+■■■■■■■■■■■■■■■■"
+echo $r"00. Keluar "
+echo $g
+read -p " root@BLANK:~# " pil;
+
+case $pil in
+1) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+
+;;
+
+2) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/IqbalzNoobs/fb-brute
+cd fb-brute
+python2 brute.py
+
+;;
+
+3) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+
+;;
+
+4) clear
+toilet -f standard " BLANK " -F gay
+pip2 install mechanize
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+
+;;
+
+5) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+
+;;
+
+6) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
+
+;;
+
+00) toilet -f standard " BLANK " -F gay
+echo $g"Keluar....."
+exit
+;;
+
+esac
+done
+done
+
+■■■■■■■■■■■■■■■■"
+echo $r"00. Keluar "
+echo $g
+read -p " root@BLANK:~# " pil;
+
+case $pil in
+1) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+
+;;
+
+2) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/IqbalzNoobs/fb-brute
+cd fb-brute
+python2 brute.py
+
+;;
+
+3) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+
+;;
+
+4) clear
+toilet -f standard " BLANK " -F gay
+pip2 install mechanize
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+
+;;
+
+5) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+
+;;
+
+6) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
+
+;;
+
+00) toilet -f standard " BLANK " -F gay
+echo $g"Keluar....."
+exit
+;;
+
+esac
+done
+done
+
+■■■■■■■■■■■■■■■■"
+echo $r"00. Keluar "
+echo $g
+read -p " root@BLANK:~# " pil;
+
+case $pil in
+1) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+
+;;
+
+2) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/IqbalzNoobs/fb-brute
+cd fb-brute
+python2 brute.py
+
+;;
+
+3) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+
+;;
+
+4) clear
+toilet -f standard " BLANK " -F gay
+pip2 install mechanize
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+
+;;
+
+5) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+
+;;
+
+6) clear
+toilet -f standard " BLANK " -F gay
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
+
+;;
+
+00) toilet -f standard " BLANK " -F gay
+echo $g"Keluar....."
+exit
+;;
+
+esac
+done
+done
+
